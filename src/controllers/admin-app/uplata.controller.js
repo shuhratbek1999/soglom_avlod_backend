2a69a2925ce5bc400c23f8b9abd8731454ef9662
shuhratbek <shuhratbekzokirjonov1119@gmail.com>
2022-10-25 19:25:30 +0500

    uplata

---
+++ src/controllers/admin-app/uplata.controller.js
@@ -0,0 +1,97 @@
+
+const HttpException = require('../../utils/HttpException.utils');
+// const status = require('../../utils/status.utils')
+const UplataModel = require('../../models/uplata.model')
+const RegionModel = require('../../models/region.model')
+const { validationResult } = require('express-validator');
+
+/******************************************************************************
+ *                              Employer Controller
+ ******************************************************************************/
+class UplateController {
+    getAll = async (req, res, next) => {
+        const model = await UplataModel.findAll(req.body);
+        res.status(200).send({
+            error: false,
+            error_code: 200,
+            message: 'Malumotlar chiqdi',
+            data: model
+        });
+    }
+
+    getOne = async (req, res, next) => {
+        this.checkValidation(req);
+        const model = await UplataModel.findOne({
+            where:{
+                id: req.params.id
+            }
+        });
+        if(!model){
+            throw new HttpException(404, 'berilgan id bo\'yicha malumot yo\'q')
+        }
+        res.status(200).send({
+            error: false,
+            error_code: 200,
+            message: 'Malumot chiqdi',
+            data: model
+        });
+    }
+   create = async (req, res, next) => {
+       this.checkValidation(req);
+       const model = await UplataModel.create(req.body);
+       res.status(200).send({
+        error: false,
+        error_code: 200,
+        message: 'Malumotlar qo\'shildi',
+        data: model
+    });
+   }
+   update = async (req, res, next) => {
+       this.checkValidation(req);
+    const model = await UplataModel.findOne({
+        where:{
+            id: req.params.id
+        }
+    });
+    model.name = req.body.name;
+    model.region_id = req.body.region_id;
+    model.save();
+    res.status(200).send({
+        error: false,
+        error_code: 200,
+        message: 'Malumotlar tahrirlandi',
+        data: model
+    });
+}
+delete = async (req, res, next) => {
+  const model = await UplataModel.destroy({
+        where:{
+          id: req.params.id
+        }
+    });
+    if(!model){
+        throw new HttpException(404, "bunday id yoq")
+    }
+    res.status(200).send({
+        error: false,
+        error_code: 200,
+        message: 'Malumot o\'chirildi',
+        data: model
+    });
+}
+    checkValidation = (req) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            throw new HttpException(400, 'Validation faild', errors);
+        }
+    }
+
+   
+}
+
+
+
+/******************************************************************************
+ *                               Export
+ ******************************************************************************/
+module.exports = new UplateController;
--- src/startup/routes.js
+++ src/startup/routes.js
@@ -22,6 +22,7 @@ const RegDoctor = require('../routes/admin-app/registerDoctor.route')
 const UploadRouter = require('../routes/admin-app/upload.route')
 const imgRouter = require('../routes/admin-app/upload_img.route')
 const arxiv = require('../routes/admin-app/registration_arxiv.route')
+const uplata = require('../routes/admin-app/uplate')
 const HttpException = require('../utils/HttpException.utils');
 
 module.exports = function(app){
@@ -52,6 +53,7 @@ module.exports = function(app){
         app.use(`/api/v1/admin-app/upload`, UploadRouter);
         app.use(`/api/v1/admin-app/uploads`, imgRouter);
         app.use(`/api/v1/admin-app/arxiv`, arxiv);
+        app.use(`/api/v1/admin-app/uplata`, uplata);
 
         app.use(`/href`, express.static('upload'));
 
