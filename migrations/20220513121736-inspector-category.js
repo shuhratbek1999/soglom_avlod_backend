bb4364a70cae12740691d358556d2233194705e0
shuhratbek1999 <zokirjonovshuhratbek1119@gmail.com>
2022-12-09 20:51:47 +0500

    price

--- migrations/20220513121736-inspector-category.js
+++ migrations/20220513121736-inspector-category.js
@@ -26,6 +26,10 @@ exports.up = function(db) {
       type:'string',
       length:300,
       notNull: true
+    },
+    citizen_price:{
+      type: 'string',
+      notNull: true
     }
   });
 };
