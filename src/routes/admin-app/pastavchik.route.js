590cd06385d6139df16f58efdbdaf9d85eda0e9c
shuhratbek1999 <zokirjonovshuhratbek1119@gmail.com>
2022-12-21 18:12:50 +0500

    pastavchik

--- src/routes/admin-app/pastavchik.route.js
+++ src/routes/admin-app/pastavchik.route.js
@@ -5,7 +5,7 @@ const auth = require('../../middleware/auth.middleware');
 // const Role = require('../../utils/roles.utils');
 const awaitHandlerFactory = require('../../middleware/awaitHandlerFactory.middleware');
 
-const  {directModels}  = require('../../middleware/validators/admin-app/directValidator.middleware');
+const  {directModels}  = require('../../middleware/validators/admin-app/pastavchikValidator.middleware');
 
 router.get('/all', auth(),  awaitHandlerFactory(pastavchikController.getAll));
 router.get('/one/:id', auth(), awaitHandlerFactory(pastavchikController.getOne));
