6bbad05227a7d01a9e557650e8e2eafddc05e325
shuhratbek1999 <zokirjonovshuhratbek1119@gmail.com>
2022-12-22 15:16:50 +0500

    prixod

--- src/middleware/validators/admin-app/ReagentValidator.middleware.js
+++ src/middleware/validators/admin-app/ReagentValidator.middleware.js
@@ -6,13 +6,5 @@ exports.reagent = [
         .isString()
         .withMessage('string typeda kiriting')
         .isLength({min: 3, max: 300})
-        .withMessage('eng kamida 4 ta harfdan iborat bolsin'),
-    body('price')
-         .exists()
-         .isDecimal()
-         .withMessage('Decimal tipida kiriting'),
-         body('count')
-         .exists()
-         .isDecimal()
-         .withMessage('Decimal tipida kiriting')
+        .withMessage('eng kamida 4 ta harfdan iborat bolsin')
 ];
