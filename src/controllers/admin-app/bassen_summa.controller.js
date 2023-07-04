fb05b8a95f65d39a515720b1a437ca73d4149d29
MuhiddinWebDev <xazratqulovmuhiddin@gmail.com>
2023-07-04 15:39:45 +0500

    fixed

--- src/controllers/admin-app/bassen_summa.controller.js
+++ src/controllers/admin-app/bassen_summa.controller.js
@@ -1,123 +1,126 @@
-
-const HttpException = require('../../utils/HttpException.utils');
+const HttpException = require("../../utils/HttpException.utils");
 // const status = require('../../utils/status.utils')
-const kirish_basseynModel = require('../../models/kirish_bassen.model');
-const kirish_basseyn = require('../../models/register_bassen.model');
-const Register_kassaModel = require('../../models/register_kassa.model');
-const { validationResult } = require('express-validator');
+const kirish_basseynModel = require("../../models/kirish_bassen.model");
+const kirish_basseyn = require("../../models/register_bassen.model");
+const Register_kassaModel = require("../../models/register_kassa.model");
+const { validationResult } = require("express-validator");
 
 /******************************************************************************
  *                              Employer Controller
  ******************************************************************************/
 class kirish_basseynController {
-    getAll = async (req, res, next) => {
-        const model = await kirish_basseynModel.findAll();
-        res.status(200).send({
-            error: false,
-            error_code: 200,
-            message: 'Malumotlar chiqdi',
-            data: model
-        });
-    }
-    kirish = async(req, res, next) => {
-        let body = req.body, date = Math.floor(new Date().getTime() / 1000),
-        filial_id = req.currentUser.filial_id;
-        let summa, sum = 0;
-        summa = await kirish_basseynModel.findOne({
-            where:{id: 1}
-        })
-        sum += summa.dataValues.price * body.odam_soni;
-         let model = await kirish_basseyn.create({
-            "date_time": date,
-            "type": "Нақт",
-            "price": body.odam_soni > 0 ? summa.dataValues.price * body.odam_soni : 0,
-            "odam_soni": body.odam_soni,
-            "doc_type": "Кирим"
-         })
-         let kassa = {
-            "date_time": date,
-            "type": 0,
-            "price": sum,
-            "pay_type": "Naqt",
-            "doc_type": "Kirim",
-            "doctor_id": model.id,
-            "place": "Бассейн",
-            "filial_id": filial_id
-       }
-       await Register_kassaModel.create(kassa);
-       res.send(model)
-       }
-    getOne = async (req, res, next) => {
-        const model = await kirish_basseynModel.findOne({
-            where:{
-                id: req.params.id
-            }
-        });
-        if(!model){
-            throw new HttpException(404, 'berilgan id bo\'yicha malumot yo\'q')
-        }
-        res.status(200).send({
-            error: false,
-            error_code: 200,
-            message: 'Malumot chiqdi',
-            data: model
-        });
-    }
-   create = async (req, res, next) => {
-       this.checkValidation(req);
-       const model = await kirish_basseynModel.create(req.body);
-       res.status(200).send({
-        error: false,
-        error_code: 200,
-        message: 'Malumotlar qo\'shildi',
-        data: model
+  getAll = async (req, res, next) => {
+    const model = await kirish_basseynModel.findAll();
+    res.status(200).send({
+      error: false,
+      error_code: 200,
+      message: "Malumotlar chiqdi",
+      data: model,
     });
-   }
-   update = async (req, res, next) => {
-       this.checkValidation(req);
+  };
+
+  kirish = async (req, res, next) => {
+    let body = req.body,
+      date = Math.floor(new Date().getTime() / 1000),
+      filial_id = req.currentUser.filial_id;
+    let summa,
+      sum = 0;
+    summa = await kirish_basseynModel.findOne({
+      where: { id: 1 },
+    });
+    sum += summa.dataValues.price * body.odam_soni;
+    let model = await kirish_basseyn.create({
+      date_time: date,
+      type: "Нақт",
+      price: body.odam_soni > 0 ? summa.dataValues.price * body.odam_soni : 0,
+      odam_soni: body.odam_soni,
+      doc_type: "Кирим",
+    });
+    let kassa = {
+      date_time: date,
+      type: 0,
+      price: sum,
+      pay_type: "Naqt",
+      doc_type: "Kirim",
+      doctor_id: model.id,
+      place: "Бассейн",
+      filial_id: filial_id,
+    };
+    await Register_kassaModel.create(kassa);
+    res.send(model);
+  };
+
+  getOne = async (req, res, next) => {
     const model = await kirish_basseynModel.findOne({
-        where:{
-            id: req.params.id
-        }
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!model) {
+      throw new HttpException(404, "berilgan id bo'yicha malumot yo'q");
+    }
+    res.status(200).send({
+      error: false,
+      error_code: 200,
+      message: "Malumot chiqdi",
+      data: model,
+    });
+  };
+
+  create = async (req, res, next) => {
+    this.checkValidation(req);
+    const model = await kirish_basseynModel.create(req.body);
+    res.status(200).send({
+      error: false,
+      error_code: 200,
+      message: "Malumotlar qo'shildi",
+      data: model,
+    });
+  };
+
+  update = async (req, res, next) => {
+    this.checkValidation(req);
+    const model = await kirish_basseynModel.findOne({
+      where: {
+        id: req.params.id,
+      },
     });
     model.price = req.body.price;
     model.save();
     res.status(200).send({
-        error: false,
-        error_code: 200,
-        message: 'Malumotlar tahrirlandi',
-        data: model
+      error: false,
+      error_code: 200,
+      message: "Malumotlar tahrirlandi",
+      data: model,
     });
-}
-delete = async (req, res, next) => {
-  const model = await kirish_basseynModel.destroy({
-        where:{
-          id: req.params.id
-        }
+  };
+
+  delete = async (req, res, next) => {
+    const model = await kirish_basseynModel.destroy({
+      where: {
+        id: req.params.id,
+      },
     });
-    if(!model){
-        throw new HttpException(404, "bunday id yoq")
+    if (!model) {
+      throw new HttpException(404, "bunday id yoq");
     }
     res.status(200).send({
-        error: false,
-        error_code: 200,
-        message: 'Malumot o\'chirildi',
-        data: model
+      error: false,
+      error_code: 200,
+      message: "Malumot o'chirildi",
+      data: model,
     });
-}
-    checkValidation = (req) => {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            throw new HttpException(400, 'Validation faild', errors);
-        }
+  };
+
+  checkValidation = (req) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new HttpException(400, "Validation faild", errors);
     }
-
-   
+  };
 }
-
-
 
 /******************************************************************************
  *                               Export
  ******************************************************************************/
-module.exports = new kirish_basseynController;
+module.exports = new kirish_basseynController();
--- src/routes/admin-app/kirish_bassen.route.js
+++ src/routes/admin-app/kirish_bassen.route.js
@@ -5,10 +5,9 @@ const auth = require('../../middleware/auth.middleware');
 // const Role = require('../../utils/roles.utils');
 const awaitHandlerFactory = require('../../middleware/awaitHandlerFactory.middleware');
 
-
 router.get('/all', auth(),  awaitHandlerFactory(BassenController.getAll));
 router.get('/one/:id', auth(), awaitHandlerFactory(BassenController.getOne));
-// router.post('/create',auth(), awaitHandlerFactory(BassenController.create));
+router.post('/create',auth(), awaitHandlerFactory(BassenController.create));
 router.post('/kirish',auth(), awaitHandlerFactory(BassenController.kirish));
 router.patch('/update/:id', auth(), awaitHandlerFactory(BassenController.update));
 router.delete('/delete/:id', auth(), awaitHandlerFactory(BassenController.delete));
