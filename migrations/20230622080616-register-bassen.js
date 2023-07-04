6eebea2f09db4c5aa22b5fbd3a5913d9719cd14e
MuhiddinWebDev <xazratqulovmuhiddin@gmail.com>
2023-07-04 16:00:28 +0500

    fixed reg_bassen name

--- migrations/20230622080616-register-bassen.js
+++ migrations/20230622080616-register-bassen.js
@@ -15,7 +15,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  return db.createTable("register_Бассейн", {
+  return db.createTable("register_bassen", {
     id:{
       type: 'int',
       notNull: true,
@@ -43,7 +43,7 @@ exports.up = function(db) {
 };
 
 exports.down = function(db) {
-  return db.dropTable("register_Бассейн");
+  return db.dropTable("register_bassen");
 };
 
 exports._meta = {
