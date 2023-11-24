7d98fe24bbef51a6d30c3e1f3c1805c9ea18f831
MuhiddinWebDev <xazratqulovmuhiddin@gmail.com>
2023-11-24 23:05:47 +0500

    fixed

--- src/controllers/admin-app/pastavchik_pay.controller.js
+++ src/controllers/admin-app/pastavchik_pay.controller.js
@@ -14,8 +14,8 @@ const pastavchikModel = require('../../models/pastavchik.model');
 class pastavchik_payController {
     getAll = async (req, res, next) => {
         const model = await pastavchik_payModel.findAll({
-            include:[
-                {model: pastavchikModel, as: 'pastavchik'}
+            include: [
+                { model: pastavchikModel, as: 'pastavchik' }
             ]
         });
         res.status(200).send({
@@ -29,21 +29,21 @@ class pastavchik_payController {
     getOne = async (req, res, next) => {
         this.checkValidation(req);
         let model = await pastavchik_payModel.findOne({
-            where:{
+            where: {
                 id: req.params.id
             },
-            include:[
-                {model: pastavchikModel, as: 'pastavchik'}
+            include: [
+                { model: pastavchikModel, as: 'pastavchik' }
             ]
         });
-        if(!model){
+        if (!model) {
             throw new HttpException(404, 'berilgan id bo\'yicha malumot yo\'q')
         }
-        if(model.dataValues.type == false){
+        if (model.dataValues.type == false) {
             model.dataValues.type = 0;
             model.dataValues.date_time = String(model.dataValues.date_time)
         }
-        else{
+        else {
             model.dataValues.type = 1;
             model.dataValues.date_time = String(model.dataValues.date_time)
         }
@@ -54,214 +54,203 @@ class pastavchik_payController {
             data: model
         });
     }
-   create = async (req, res, next) => {
-       this.checkValidation(req);
-       let filial_id = req.currentUser.filial_id;
-       const model = await pastavchik_payModel.create({
-          "type": req.body.type,
-          "price": req.body.price,
-          "backlog": req.body.backlog,
-          "jami_summa": req.body.jami_summa,
-          "pastavchik_id": req.body.pastavchik_id,
-          "filial_id": filial_id,
-          "date_time": Math.floor(new Date().getTime() / 1000)
-       });
-       var register = {
-        "date_time": Math.floor(new Date().getTime() / 1000),
-        "doc_id": model.id,
-        "summa": model.price,
-        "doc_type": "kirim",
-        "type": model.type,
-        "place": "поставщик",
-        "pastavchik_id": model.pastavchik_id,
-        "filial_id": filial_id
-      }
-      await register_supplierModel.create(register);
-      var kassa = {
-        "date_time": Math.floor(new Date().getTime() / 1000),
-        "doctor_id": model.id,
-        "price": model.price,
-        "doc_type": "Chiqim",
-        "pay_type": model.type == 0 ? "Naqt" : "Plastik",
-        "type": model.type,
-        "place": "поставщик",
-        "filial_id": filial_id
-      }
-         await register_kassaModel.create(kassa)
-       res.status(200).send({
-        error: false,
-        error_code: 200,
-        message: 'Malumotlar qo\'shildi',
-        data: model
-    });
-   }
-   update = async (req, res, next) => {
-       this.checkValidation(req);
-       let filial_id = req.currentUser.filial_id;
-    const model = await pastavchik_payModel.findOne({
-        where:{
-            id: req.params.id
+    create = async (req, res, next) => {
+        this.checkValidation(req);
+        let filial_id = req.currentUser.filial_id;
+        const model = await pastavchik_payModel.create({
+            "type": req.body.type,
+            "price": req.body.price,
+            "backlog": req.body.backlog,
+            "jami_summa": req.body.jami_summa,
+            "pastavchik_id": req.body.pastavchik_id,
+            "filial_id": filial_id,
+            "date_time": Math.floor(new Date().getTime() / 1000)
+        });
+        var register = {
+            "date_time": Math.floor(new Date().getTime() / 1000),
+            "doc_id": model.id,
+            "summa": model.price,
+            "doc_type": "kirim",
+            "type": model.type,
+            "place": "поставщик",
+            "pastavchik_id": model.pastavchik_id,
+            "filial_id": filial_id
         }
-    });
-    model.type = req.body.type;
-    model.price = req.body.price;
-    model.backlog = req.body.backlog;
-    model.jami_summa = req.body.jami_summa;
-    model.pastavchik_id = req.body.pastavchik_id;
-    model.filial_id = filial_id;
-    model.date_time = Math.floor(new Date().getTime() / 1000);
-    model.save();
-    await register_supplierModel.destroy({
-        where:{
-            doc_id: model.id,
-            place: 'поставщик'
+        await register_supplierModel.create(register);
+        var kassa = {
+            "date_time": Math.floor(new Date().getTime() / 1000),
+            "doctor_id": model.id,
+            "price": model.price,
+            "doc_type": "Chiqim",
+            "pay_type": model.type == 0 ? "Naqt" : "Plastik",
+            "type": model.type,
+            "place": "поставщик",
+            "filial_id": filial_id
         }
-    })
-    var register = {
-        "date_time": Math.floor(new Date().getTime() / 1000),
-        "doc_id": model.id,
-        "summa": model.price,
-        "doc_type": "kirim",
-        "type": model.type,
-        "place": "поставщик",
-        "pastavchik_id": model.pastavchik_id,
-        "filial_id": filial_id,
-      }
-      await register_supplierModel.create(register);
-      await register_kassaModel.destroy({
-        where:{
-            doctor_id: model.id,
-            place: 'поставщик'
+        await register_kassaModel.create(kassa)
+        res.status(200).send({
+            error: false,
+            error_code: 200,
+            message: 'Malumotlar qo\'shildi',
+            data: model
+        });
+    }
+    update = async (req, res, next) => {
+        this.checkValidation(req);
+        let filial_id = req.currentUser.filial_id;
+        const model = await pastavchik_payModel.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        model.type = req.body.type;
+        model.price = req.body.price;
+        model.backlog = req.body.backlog;
+        model.jami_summa = req.body.jami_summa;
+        model.pastavchik_id = req.body.pastavchik_id;
+        model.filial_id = filial_id;
+        model.date_time = Math.floor(new Date().getTime() / 1000);
+        model.save();
+        await register_supplierModel.destroy({
+            where: {
+                doc_id: model.id,
+                place: 'поставщик'
+            }
+        })
+        var register = {
+            "date_time": Math.floor(new Date().getTime() / 1000),
+            "doc_id": model.id,
+            "summa": model.price,
+            "doc_type": "kirim",
+            "type": model.type,
+            "place": "поставщик",
+            "pastavchik_id": model.pastavchik_id,
+            "filial_id": filial_id,
         }
-      })
-      var kassa = {
-        "date_time": Math.floor(new Date().getTime() / 1000),
-        "doctor_id": model.id,
-        "price": model.price,
-        "doc_type": "Chiqim",
-        "pay_type": model.type == 0 ? "Naqt" : "Plastik",
-        "type": model.type,
-        "place": "поставщик",
-        "filial_id": filial_id
-      }
-         await register_kassaModel.create(kassa)
-    res.status(200).send({
-        error: false,
-        error_code: 200,
-        message: 'Malumotlar tahrirlandi',
-        data: model
-    });
-}
+        await register_supplierModel.create(register);
+        await register_kassaModel.destroy({
+            where: {
+                doctor_id: model.id,
+                place: 'поставщик'
+            }
+        })
+        var kassa = {
+            "date_time": Math.floor(new Date().getTime() / 1000),
+            "doctor_id": model.id,
+            "price": model.price,
+            "doc_type": "Chiqim",
+            "pay_type": model.type == 0 ? "Naqt" : "Plastik",
+            "type": model.type,
+            "place": "поставщик",
+            "filial_id": filial_id
+        }
+        await register_kassaModel.create(kassa)
+        res.status(200).send({
+            error: false,
+            error_code: 200,
+            message: 'Malumotlar tahrirlandi',
+            data: model
+        });
+    }
 
- pastavchikHisobot = async(req, res, next) => {
-    let body = req.body; 
-    let query = {}, queryx = {};
+    pastavchikHisobot = async (req, res, next) => {
+        let body = req.body;
+        let query = {}, queryx = {};
         let datetime1 = body.datetime1;
         let datetime2 = body.datetime2;
-        if(body.pastavchik_id !== null){
-            query.id = {[Op.eq] : body.pastavchik_id }  
-            queryx.pastavchik_id = {[Op.eq]: body.pastavchik_id}
-            queryx.filial_id = {[Op.eq]: body.filial_id}
+        if (body.pastavchik_id !== null) {
+            query.id = { [Op.eq]: body.pastavchik_id }
+            queryx.pastavchik_id = { [Op.eq]: body.pastavchik_id }
+            queryx.filial_id = { [Op.eq]: body.filial_id }
         }
-        else{
-            queryx.filial_id = {[Op.eq]: body.filial_id}
+        else {
+            queryx.filial_id = { [Op.eq]: body.filial_id }
         }
-    let model = await register_supplierModel.findAll({
-        attributes : [ 
-            'id', 'doc_id', "type", "date_time", "doc_type", "summa", "pastavchik_id", "place",
-            [sequelize.literal("SUM(CASE WHEN register_supplier.date_time < " + datetime1 + " THEN register_supplier.summa * power(-1, 'type') ELSE 0 END)"), 'total'],
-            [sequelize.literal("SUM(CASE WHEN register_supplier.date_time >= " + datetime1 + " and register_supplier.date_time <= " + datetime2 + " AND register_supplier.doc_type = 'kirim' THEN register_supplier.summa ELSE 0 END)"), 'total_kirim'],
-            [sequelize.literal("SUM(CASE WHEN register_supplier.date_time >= " + datetime1 + " and register_supplier.date_time <= " + datetime2 + " AND register_supplier.doc_type = 'chiqim' THEN register_supplier.summa ELSE 0 END)"), 'total_chiqim'],
-            [sequelize.literal("SUM(CASE WHEN register_supplier.date_time < " + datetime2 + " THEN register_supplier.summa * power(-1, 'type') ELSE 0 END)"), 'end_total'],
-        ],
-        include:[
-            {model: pastavchikModel, as: 'pastavchik'}
-        ],
-        where: queryx,
-        group: ['pastavchik_id']
-    })
-    res.send(model)
- } 
-  getPastavchik = async(req, res, next) => {
-    let prixod = await register_supplierModel.findAll({
-        where:{
-            pastavchik_id: req.body.pastavchik_id,
-            place: 'Prixod'
-        },
-        raw: true
-    })
-    let pastavchik = await register_supplierModel.findAll({
-            where:{
-                pastavchik_id: req.body.pastavchik_id,
-                place: 'Pastavchik'
+        let model = await register_supplierModel.findAll({
+            attributes: [
+                'id', 'doc_id', "type", "date_time", "doc_type", "summa", "pastavchik_id", "place",
+                [sequelize.literal("SUM(CASE WHEN register_supplier.date_time < " + datetime1 + " THEN register_supplier.summa * power(-1, 'type') ELSE 0 END)"), 'total'],
+                [sequelize.literal("SUM(CASE WHEN register_supplier.date_time >= " + datetime1 + " and register_supplier.date_time <= " + datetime2 + " AND register_supplier.doc_type = 'kirim' THEN register_supplier.summa ELSE 0 END)"), 'total_kirim'],
+                [sequelize.literal("SUM(CASE WHEN register_supplier.date_time >= " + datetime1 + " and register_supplier.date_time <= " + datetime2 + " AND register_supplier.doc_type = 'chiqim' THEN register_supplier.summa ELSE 0 END)"), 'total_chiqim'],
+                [sequelize.literal("SUM(CASE WHEN register_supplier.date_time < " + datetime2 + " THEN register_supplier.summa * power(-1, 'type') ELSE 0 END)"), 'end_total'],
+            ],
+            include: [
+                { model: pastavchikModel, as: 'pastavchik' }
+            ],
+            where: queryx,
+            group: ['pastavchik_id']
+        })
+        res.send(model)
+    }
+    getPastavchik = async (req, res, next) => {
+        let pastavchikID = req.body.pastavchik_id;
+        let umumiy = 0;
+        let model = await register_supplierModel.findOne({
+            attributes:[
+                [sequelize.literal("SUM(CASE WHEN type = 2 THEN summa ELSE 0 END)"),'kirim'],
+                [sequelize.literal("SUM(CASE WHEN type = 0 THEN summa ELSE 0 END)"),'chiqim'],
+            ],
+            where: {
+                pastavchik_id: pastavchikID,
             },
             raw: true
-    })
-    let umumiy, sum1, sum2;
-    function prixodSumma(prixod){
-        sum1 = 0;
-        for(let key of prixod){
-            sum1 += key.summa;
+        })
+        console.log(model)
+        if(model){
+            if(model.kirim > model.chiqim){
+                umumiy = model.kirim - model.chiqim;
+            }else{
+                umumiy = model.chiqim - model.kirim;
+            }
         }
-        return sum1;
+        
+        res.send({
+            "backlog": umumiy
+        })
     }
-    prixodSumma(prixod);
-    function pastavchikSumma(pastavchik){
-         sum2 = 0;
-         for(let key of pastavchik){
-            sum2 += key.summa;
-         }
-         return sum2;
-    }
-    pastavchikSumma(pastavchik);
-    umumiy = sum2 - sum1;
-     res.send({
-        "backlog": umumiy
-     })
-  }
- pastavchikSverka = async(req, res, next) => {
-    let body = req.body; 
-    let query = {}, queryx = {};
+    pastavchikSverka = async (req, res, next) => {
+        let body = req.body;
+        let query = {}, queryx = {};
         let datetime1 = body.datetime1;
         let datetime2 = body.datetime2;
-        if(body.pastavchik_id !== null){
-            query.id = {[Op.eq] : body.pastavchik_id }  
-            queryx.pastavchik_id = {[Op.eq]: body.pastavchik_id}
-            queryx.filial_id = {[Op.eq]: body.filial_id}
+        if (body.pastavchik_id !== null) {
+            query.id = { [Op.eq]: body.pastavchik_id }
+            queryx.pastavchik_id = { [Op.eq]: body.pastavchik_id }
+            queryx.filial_id = { [Op.eq]: body.filial_id }
         }
-        else{
-            queryx.filial_id = {[Op.eq]: body.filial_id}
+        else {
+            queryx.filial_id = { [Op.eq]: body.filial_id }
         }
-    let model = await register_supplierModel.findAll({
-        attributes : [ 
-            'id', 'pastavchik_id', "type", "date_time", "doc_type", "summa", "doc_id", "place",
-            [sequelize.literal("SUM(CASE WHEN register_supplier.date_time < " + datetime1 + " THEN register_supplier.summa * power(-1, 'type') ELSE 0 END)"), 'total'],
-            [sequelize.literal("SUM(CASE WHEN register_supplier.date_time >= " + datetime1 + " and register_supplier.date_time <= " + datetime2 + " AND register_supplier.doc_type = 'kirim' THEN register_supplier.summa ELSE 0 END)"), 'total_kirim'],
-            [sequelize.literal("SUM(CASE WHEN register_supplier.date_time >= " + datetime1 + " and register_supplier.date_time <= " + datetime2 + " AND register_supplier.doc_type = 'chiqim' THEN register_supplier.summa ELSE 0 END)"), 'total_chiqim'],
-            [sequelize.literal("SUM(CASE WHEN register_supplier.date_time < " + datetime2 + " THEN register_supplier.summa * power(-1, 'type') ELSE 0 END)"), 'end_total'],
-        ],
-        where: queryx,
-        group: ['id']
-    })
-    res.send(model)
- } 
-
-delete = async (req, res, next) => {
-  const model = await pastavchik_payModel.destroy({
-        where:{
-          id: req.params.id
-        }
-    });
-    if(!model){
-        throw new HttpException(404, "bunday id yoq")
+        let model = await register_supplierModel.findAll({
+            attributes: [
+                'id', 'pastavchik_id', "type", "date_time", "doc_type", "summa", "doc_id", "place",
+                [sequelize.literal("SUM(CASE WHEN register_supplier.date_time < " + datetime1 + " THEN register_supplier.summa * power(-1, 'type') ELSE 0 END)"), 'total'],
+                [sequelize.literal("SUM(CASE WHEN register_supplier.date_time >= " + datetime1 + " and register_supplier.date_time <= " + datetime2 + " AND register_supplier.doc_type = 'kirim' THEN register_supplier.summa ELSE 0 END)"), 'total_kirim'],
+                [sequelize.literal("SUM(CASE WHEN register_supplier.date_time >= " + datetime1 + " and register_supplier.date_time <= " + datetime2 + " AND register_supplier.doc_type = 'chiqim' THEN register_supplier.summa ELSE 0 END)"), 'total_chiqim'],
+                [sequelize.literal("SUM(CASE WHEN register_supplier.date_time < " + datetime2 + " THEN register_supplier.summa * power(-1, 'type') ELSE 0 END)"), 'end_total'],
+            ],
+            where: queryx,
+            group: ['id']
+        })
+        res.send(model)
     }
-    res.status(200).send({
-        error: false,
-        error_code: 200,
-        message: 'Malumot o\'chirildi',
-        data: model
-    });
-}
+
+    delete = async (req, res, next) => {
+        const model = await pastavchik_payModel.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        if (!model) {
+            throw new HttpException(404, "bunday id yoq")
+        }
+        res.status(200).send({
+            error: false,
+            error_code: 200,
+            message: 'Malumot o\'chirildi',
+            data: model
+        });
+    }
     checkValidation = (req) => {
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
@@ -269,7 +258,7 @@ delete = async (req, res, next) => {
         }
     }
 
-   
+
 }
 
 
