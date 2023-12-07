59d0918a7748b4b190d082b9d57f2277c249d50b
MuhiddinWebDev <xazratqulovmuhiddin@gmail.com>
2023-12-07 12:53:46 +0500

    fixed

--- src/controllers/admin-app/cron.js
+++ src/controllers/admin-app/cron.js
@@ -9,109 +9,109 @@ cron.timezone = 'Asia/Tashkent';
 
 // 30 23 * * *
 module.exports = async function() {
-    cron.schedule("1 23 * * *", async () => {
-        try {
-        //  const tz = 'Asia/Tashkent';
-        //  const unixtime = moment.tz(tz).unix();
-        //  let Ambulator = await Model.findAll({
-        //      where:{
-        //          type_service: 'Ambulator',
-        //          created_at: {[Op.lt]: unixtime}
-        //      }
-        //  });
+    // cron.schedule("1 1 * * *", async () => {
+    //     try {
+    //         console.log('Test___________--')
+    //      const tz = 'Asia/Tashkent';
+    //      const unixtime = moment.tz(tz).unix();
+    //      let Ambulator = await Model.findAll({
+    //          where:{
+    //              type_service: 'Ambulator',
+    //          }
+    //      });
+    //     console.log(Ambulator)
+    //      if(Ambulator.length > 0){
+    //          Ambulator.forEach(async item => {
+    //              if(item.created_at < unixtime){
+    //                  await db.query(`INSERT INTO registration_inspection_child_arxiv SELECT * FROM registration_inspection_child where registration_id = ${item.dataValues.id}`); 
+    //                  await db.query(`INSERT INTO registration_inspection_arxiv SELECT * FROM registration_inspection where registration_id = ${item.dataValues.id}`);
+    //                  await db.query(`INSERT INTO registration_files_arxiv SELECT * FROM registration_files where registration_id = ${item.dataValues.id}`);
+    //                  await db.query(`INSERT INTO registration_recipe_arxiv SELECT * FROM registration_recipe where registration_id = ${item.dataValues.id}`);
+    //                  await db.query(`INSERT INTO registration_doctor_arxiv SELECT * FROM registration_doctor where registration_id = ${item.dataValues.id}`);
+    //                  await db.query(`INSERT INTO registration_arxiv SELECT * FROM registration where id = ${item.dataValues.id}`);
+    //                  await db.query(`INSERT INTO registration_pay_arxiv SELECT * FROM registration_pay where registration_id = ${item.dataValues.id}`);
+    //                  await db.query(`INSERT INTO registration_palata_arxiv SELECT * FROM registration_palata where registration_id = ${item.dataValues.id}`);
  
-        //  if(Ambulator.length > 0){
-        //      Ambulator.forEach(async item => {
-        //          if(item.created_at < unixtime){
-        //              await db.query(`INSERT INTO registration_inspection_child_arxiv SELECT * FROM registration_inspection_child where registration_id = ${item.dataValues.id}`); 
-        //              await db.query(`INSERT INTO registration_inspection_arxiv SELECT * FROM registration_inspection where registration_id = ${item.dataValues.id}`);
-        //              await db.query(`INSERT INTO registration_files_arxiv SELECT * FROM registration_files where registration_id = ${item.dataValues.id}`);
-        //              await db.query(`INSERT INTO registration_recipe_arxiv SELECT * FROM registration_recipe where registration_id = ${item.dataValues.id}`);
-        //              await db.query(`INSERT INTO registration_doctor_arxiv SELECT * FROM registration_doctor where registration_id = ${item.dataValues.id}`);
-        //              await db.query(`INSERT INTO registration_arxiv SELECT * FROM registration where id = ${item.dataValues.id}`);
-        //              await db.query(`INSERT INTO registration_pay_arxiv SELECT * FROM registration_pay where registration_id = ${item.dataValues.id}`);
-        //              await db.query(`INSERT INTO registration_palata_arxiv SELECT * FROM registration_palata where registration_id = ${item.dataValues.id}`);
- 
-        //              // get all records from original table
+    //                  // get all records from original table
                      
-        //              await db.query(`DELETE from registration_inspection where registration_id = ${item.dataValues.id}`);
-        //              await db.query(`DELETE from registration_inspection_child where registration_id = ${item.dataValues.id}`);
-        //              await db.query(`DELETE from registration_files where registration_id = ${item.dataValues.id}`);
-        //              await db.query(`DELETE from registration_recipe where registration_id = ${item.dataValues.id}`);
-        //              await db.query(`DELETE from registration_doctor where registration_id = ${item.dataValues.id}`);
-        //              await db.query(`DELETE from registration where id = ${item.dataValues.id}`);
-        //              await db.query(`DELETE from registration_pay where registration_id = ${item.dataValues.id}`);
-        //              await db.query(`DELETE from registration_palata where registration_id = ${item.dataValues.id}`);
+    //                  await db.query(`DELETE from registration_inspection where registration_id = ${item.dataValues.id}`);
+    //                  await db.query(`DELETE from registration_inspection_child where registration_id = ${item.dataValues.id}`);
+    //                  await db.query(`DELETE from registration_files where registration_id = ${item.dataValues.id}`);
+    //                  await db.query(`DELETE from registration_recipe where registration_id = ${item.dataValues.id}`);
+    //                  await db.query(`DELETE from registration_doctor where registration_id = ${item.dataValues.id}`);
+    //                  await db.query(`DELETE from registration where id = ${item.dataValues.id}`);
+    //                  await db.query(`DELETE from registration_pay where registration_id = ${item.dataValues.id}`);
+    //                  await db.query(`DELETE from registration_palata where registration_id = ${item.dataValues.id}`);
  
-        //              await db.query(`TRUNCATE TABLE queue`);
-        //          }
-        //          else{
-        //              console.log("oldin kelgan bemorlar mavjud emas");
-        //          }
-        //      })
-        //  }
+    //                  await db.query(`TRUNCATE TABLE queue`);
+    //              }
+    //              else{
+    //                  console.log("oldin kelgan bemorlar mavjud emas");
+    //              }
+    //          })
+    //      }
  
-        //  const sql = `
-        //          SELECT 
-        //              s.*,
-        //              rp.*
-        //          FROM 
-        //              registration s
-        //          LEFT JOIN 
-        //              register_palata rp ON s.id = rp.registration_id  
-        //          WHERE 
-        //              s.type_service = 'Statsionar' AND
-        //              s.backlog <= 0
-        //          `;
+    //     //  const sql = `
+    //     //          SELECT 
+    //     //              s.*,
+    //     //              rp.*
+    //     //          FROM 
+    //     //              registration s
+    //     //          LEFT JOIN 
+    //     //              register_palata rp ON s.id = rp.registration_id  
+    //     //          WHERE 
+    //     //              s.type_service = 'Statsionar' AND
+    //     //              s.backlog <= 0
+    //     //          `;
  
-        //      const Statsionar = await db.query(sql, {
-        //          type: QueryTypes.SELECT
-        //      });
+    //     //      const Statsionar = await db.query(sql, {
+    //     //          type: QueryTypes.SELECT
+    //     //      });
  
          
-        //  let thisDay = Math.floor(new Date().getTime() / 1000)
+    //     //  let thisDay = Math.floor(new Date().getTime() / 1000)
  
  
-        //  if(Statsionar.length > 0) {
-        //      Statsionar.forEach(async item => {
+    //     //  if(Statsionar.length > 0) {
+    //     //      Statsionar.forEach(async item => {
  
-        //          if(item.backlog <= 0 && item.date_do >= thisDay){
-        //              await db.query(`INSERT INTO registration_inspection_child_arxiv SELECT * FROM registration_inspection_child where registration_id = ${item.id}`);
-        //              await db.query(`DELETE from registration_inspection_child where registration_id = ${item.id}`);
+    //     //          if(item.backlog <= 0 && item.date_do >= thisDay){
+    //     //              await db.query(`INSERT INTO registration_inspection_child_arxiv SELECT * FROM registration_inspection_child where registration_id = ${item.id}`);
+    //     //              await db.query(`DELETE from registration_inspection_child where registration_id = ${item.id}`);
  
-        //              await db.query(`INSERT INTO registration_inspection_arxiv SELECT * FROM registration_inspection where registration_id = ${item.id}`);
-        //              await db.query(`DELETE from registration_inspection where registration_id = ${item.id}`);
+    //     //              await db.query(`INSERT INTO registration_inspection_arxiv SELECT * FROM registration_inspection where registration_id = ${item.id}`);
+    //     //              await db.query(`DELETE from registration_inspection where registration_id = ${item.id}`);
  
-        //              await db.query(`INSERT INTO registration_files_arxiv SELECT * FROM registration_files where registration_id = ${item.id}`);
-        //              await db.query(`DELETE from registration_files where registration_id = ${item.id}`);
+    //     //              await db.query(`INSERT INTO registration_files_arxiv SELECT * FROM registration_files where registration_id = ${item.id}`);
+    //     //              await db.query(`DELETE from registration_files where registration_id = ${item.id}`);
  
-        //              await db.query(`INSERT INTO registration_recipe_arxiv SELECT * FROM registration_recipe where registration_id = ${item.id}`);
-        //              await db.query(`DELETE from registration_recipe where registration_id = ${item.id}`);
+    //     //              await db.query(`INSERT INTO registration_recipe_arxiv SELECT * FROM registration_recipe where registration_id = ${item.id}`);
+    //     //              await db.query(`DELETE from registration_recipe where registration_id = ${item.id}`);
  
-        //              await db.query(`INSERT INTO registration_doctor_arxiv SELECT * FROM registration_doctor where registration_id = ${item.id}`);
-        //              await db.query(`DELETE from registration_doctor where registration_id = ${item.id}`);
+    //     //              await db.query(`INSERT INTO registration_doctor_arxiv SELECT * FROM registration_doctor where registration_id = ${item.id}`);
+    //     //              await db.query(`DELETE from registration_doctor where registration_id = ${item.id}`);
  
-        //              await db.query(`INSERT INTO registration_arxiv SELECT * FROM registration where id = ${item.id}`);
-        //              await db.query(`DELETE from registration where id = ${item.id}`);
+    //     //              await db.query(`INSERT INTO registration_arxiv SELECT * FROM registration where id = ${item.id}`);
+    //     //              await db.query(`DELETE from registration where id = ${item.id}`);
  
-        //              await db.query(`INSERT INTO registration_pay_arxiv SELECT * FROM registration_pay where registration_id = ${item.id}`);
-        //              await db.query(`DELETE from registration_pay where registration_id = ${item.id}`);
+    //     //              await db.query(`INSERT INTO registration_pay_arxiv SELECT * FROM registration_pay where registration_id = ${item.id}`);
+    //     //              await db.query(`DELETE from registration_pay where registration_id = ${item.id}`);
  
-        //              await db.query(`INSERT INTO registration_palata_arxiv SELECT * FROM registration_palata where registration_id = ${item.id}`);
-        //              await db.query(`DELETE from registration_palata where registration_id = ${item.id}`);
+    //     //              await db.query(`INSERT INTO registration_palata_arxiv SELECT * FROM registration_palata where registration_id = ${item.id}`);
+    //     //              await db.query(`DELETE from registration_palata where registration_id = ${item.id}`);
  
-        //              await db.query(`INSERT INTO register_palata_arxiv SELECT * FROM register_palata where registration_id = ${item.id}`);
-        //              await db.query(`DELETE from register_palata_arxiv where registration_id = ${item.id}`);
+    //     //              await db.query(`INSERT INTO register_palata_arxiv SELECT * FROM register_palata where registration_id = ${item.id}`);
+    //     //              await db.query(`DELETE from register_palata_arxiv where registration_id = ${item.id}`);
  
-        //              await db.query(`TRUNCATE TABLE queue`);
-        //          }
-        //          else{
-        //              console.log("oldin kelgan bemorlar mavjud emas");
-        //          }
-        //      })
-        //  }
-        } catch (error) {
-         console.log(error)
-        }
-     });
+    //     //              await db.query(`TRUNCATE TABLE queue`);
+    //     //          }
+    //     //          else{
+    //     //              console.log("oldin kelgan bemorlar mavjud emas");
+    //     //          }
+    //     //      })
+    //     //  }
+    //     } catch (error) {
+    //      console.log(error)
+    //     }
+    //  });
 }
